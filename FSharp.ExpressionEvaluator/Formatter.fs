eee14ce2e2205737d256e5e9e5c5345bd3a35acb
Jakub Majocha <majocha@outlook.com>
2024-01-13 13:10:01 +0100

    Add project files.

---
+++ FSharp.ExpressionEvaluator/Formatter.fs
@@ -0,0 +1,18 @@
+﻿namespace FSharp.ExpressionEvaluator
+
+open Microsoft.VisualStudio.Debugger.ComponentInterfaces
+
+type Formatter() =
+    interface IDkmClrFormatter with
+
+        member _.GetTypeName(inspectionContext, clrType, customTypeInfo, formatSpecifiers) =
+            inspectionContext.GetTypeName(clrType, customTypeInfo, formatSpecifiers) + " ❤️!"
+
+        member _.GetUnderlyingString(clrValue, inspectionContext) =
+            clrValue.GetUnderlyingString(inspectionContext) + " ❤️!"
+
+        member _.GetValueString(clrValue, inspectionContext, formatSpecifiers) =
+            clrValue.GetValueString(inspectionContext, formatSpecifiers) + " ❤️!"
+
+        member _.HasUnderlyingString(clrValue, inspectionContext) =
+            clrValue.HasUnderlyingString inspectionContext
